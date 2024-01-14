f2a24f673ee81da336ba6c0f6a8aa43888b1a319
H4zlq <haziqnaim72@gmail.com>
2024-01-14 18:17:29 +0800

    Refactor database initialization code

--- databases/database.py
+++ databases/database.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from databases.database_meta import DatabaseMeta
 from utils.file_util import FileUtil
+from constants.constant import init_script
 
 
 class Database(metaclass=DatabaseMeta):
@@ -9,7 +10,7 @@ class Database(metaclass=DatabaseMeta):
     def get_connection(self):
         try:
             self._connection = mysql.connector.connect(
-                host="localhost", user="root", password="admin", database="bank_system"
+                host="localhost", user="root", password="admin", database="python_bank"
             )
 
             return self._connection
@@ -17,7 +18,7 @@ class Database(metaclass=DatabaseMeta):
             print(f"Cannot connect to database: {err}")
 
     def get_cursor(self):
-        return self._connection.cursor()
+        return self._connection.cursor(prepared=True)
 
     def commit(self):
         self._connection.commit()
@@ -25,13 +26,15 @@ class Database(metaclass=DatabaseMeta):
     def init(self):
         try:
             # Get connection
-            connection = self.get_connection()
+            connection = mysql.connector.connect(
+                host="localhost", user="root", password="admin"
+            )
 
             # Get cursor
-            cursor = self.get_cursor()
+            cursor = connection.cursor()
 
             # Read sql file
-            sql_file = FileUtil.read_file("scripts/init.sql")
+            sql_file = FileUtil.read_file(init_script)
 
             # Split the sql file into individual statements
             splitted_file = sql_file.split(";")
@@ -45,5 +48,8 @@ class Database(metaclass=DatabaseMeta):
 
             # Close cursor
             cursor.close()
+
+            # Close connection
+            connection.close()
         except mysql.connector.Error as err:
             print(f"Cannot initialize database: {err}")
