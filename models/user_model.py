c1b6da104449090fd2369a7244a8e9f64fd52f04
H4zlq <haziqnaim72@gmail.com>
2024-01-14 18:17:46 +0800

    Add id and account_type_id to UserModel constructor and implement getter methods

--- models/user_model.py
+++ models/user_model.py
@@ -1,14 +1,22 @@
 class UserModel:
-    def __init__(self, username, password, balance):
+    def __init__(self, id, username, password, account_type_id, balance):
+        self.id = id
         self.username = username
         self.password = password
+        self.account_type_id = account_type_id
         self.balance = balance
+
+    def get_id(self):
+        return self.id
 
     def get_username(self):
         return self.username
 
     def get_password(self):
         return self.password
+
+    def get_account_type_id(self):
+        return self.account_type_id
 
     def get_balance(self):
         return self.balance
