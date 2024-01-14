1717062e550635170f849de3eb51d7dcb0f4744b
H4zlq <haziqnaim72@gmail.com>
2024-01-14 18:17:37 +0800

    Add TransactionModel class

---
+++ models/transaction_model.py
@@ -0,0 +1,18 @@
+class TransactionModel:
+    def __init__(self, user_id, transaction_type, amount, transaction_date):
+        self._user_id = user_id
+        self._transaction_type = transaction_type
+        self._amount = amount
+        self._transaction_date = transaction_date
+
+    def get_user_id(self):
+        return self._user_id
+
+    def get_transaction_type(self):
+        return self._transaction_type
+
+    def get_amount(self):
+        return self._amount
+
+    def get_transaction_date(self):
+        return self._transaction_date
