31e798a8439e611fed48184b729c42ea2099579d
H4zlq <haziqnaim72@gmail.com>
2024-01-15 17:21:12 +0800

    Refactor main script to use a Main class and encapsulate database and chooser view instances

--- __main__.py
+++ __main__.py
@@ -1,16 +1,27 @@
 from databases.database import Database
 from views.chooser_view import ChooserView
 
+
+class Main:
+    def __init__(self):
+        pass
+
+    def run(self):
+        # Create database and chooser view instances
+        database = Database()
+        chooser_view = ChooserView()
+
+        # Initialize database
+        database.init()
+
+        # Run chooser view
+        while True:
+            chooser_view.main_menu()
+
+
 if __name__ == "__main__":
-    # Create database instance
-    database = Database()
+    # Create main instance
+    main = Main()
 
-    # Initialize database
-    database.init()
-
-    # Create chooser view instance
-    chooser_view = ChooserView()
-
-    # Run chooser view
-    while True:
-        chooser_view.main_menu()
+    # Run main
+    main.run()
