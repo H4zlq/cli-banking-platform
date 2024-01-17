951d84501f862139723597e36bb722b2f4885902
H4zlq <haziqnaim72@gmail.com>
2024-01-17 16:36:22 +0800

    Refactor session timer handling in SessionController

--- controllers/session_controller.py
+++ controllers/session_controller.py
@@ -8,26 +8,26 @@ class SessionController(metaclass=SingletonMeta):
 
     def __init__(self, authentication_view=None):
         self.authentication_view = authentication_view
-        self.timer = None
 
     def get_session(self):
         return self.session_service.get_session()
 
     def start_session(self, data):
         self.session_service.set_session(data)
-        if self.timer:
-            self.timer.cancel()
+
+        if self.session_service.timer:
+            self.session_service.timer.cancel()
+
         self.timer = threading.Timer(
             self.session_service.session_duration, self.end_session
         )
+
         self.timer.start()
 
     def end_session(self):
         self.session_service.end_session()
 
         if self.authentication_view:
-            print("\nSession has ended due to timeout.")
-            return self.authentication_view.login()
+            self.authentication_view.login()
 
-    def cancel_timer(self):
-        self.session_service.cancel_timer()
+        print("\nSession has ended due to timeout.")
